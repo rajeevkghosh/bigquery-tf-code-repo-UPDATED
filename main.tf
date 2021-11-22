e68161d057cd21befbaf0a760ef23e76aaeede91
Rajeev Ghosh <rajeevkghosh@gmail.com>
2021-11-22 17:51:55 +0530

    Modified main.tf

--- main.tf
+++ main.tf
@@ -4,14 +4,14 @@ provider "google" {
 }
 
 resource "google_bigquery_dataset" "dataset" {
-  dataset_id                  = "us_dev_abcd_fghi_dataset2"
+  dataset_id                  = "dev_abcd_fghi_dataset2"
   friendly_name               = "test"
   description                 = "This is a test description"
   location                    = "US"
   default_table_expiration_ms = 3600000
   labels = {
     env                  = "default"
-    application_division = "pci",
+   # application_division = "pci",
     application_name     = "demo",
     application_role     = "app",
     au                   = "0223092",
@@ -50,6 +50,6 @@ resource "google_kms_crypto_key" "crypto_key" {
 }
 
 resource "google_kms_key_ring" "key_ring" {
-  name     = "us-dev-abcd-fghi-keyring3"
+  name     = "us-dev-abcd-fghi-keyring4"
   location = "us"
 }

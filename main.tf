8319407b318357512814572760e7ff893d4a83cc
Rajeev Ghosh <rajeevkghosh@gmail.com>
2021-11-22 12:20:57 +0530

    Modified main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  project = "airline1-sabre-wolverine"
+  project      = "airline1-sabre-wolverine"
   access_token = var.access_token
 }
 
@@ -9,6 +9,20 @@ resource "google_bigquery_dataset" "dataset" {
   description                 = "This is a test description"
   location                    = "US"
   default_table_expiration_ms = 3600000
+  labels = {
+    env                  = "default"
+    application_division = "pci",
+    application_name     = "demo",
+    application_role     = "app",
+    au                   = "0223092",
+    created              = "YYYYMMDD",
+    data_compliance      = "pci",
+    data_confidentiality = "pub",
+    data_type            = "test",
+    environment          = "dev",
+    gcp_region           = "US",
+    owner                = "hybridenv",
+  }
 
   default_encryption_configuration {
     kms_key_name = google_kms_crypto_key.crypto_key.id
@@ -16,7 +30,22 @@ resource "google_bigquery_dataset" "dataset" {
 }
 
 resource "google_kms_crypto_key" "crypto_key" {
-  name     = "us-dev-abcd-fghi-cryptokey"
+  name = "us-dev-abcd-fghi-cryptokey"
+  labels = {
+    env                  = "default"
+    application_division = "pci",
+    application_name     = "demo",
+    application_role     = "app",
+    au                   = "0223092",
+    created              = "YYYYMMDD",
+    data_compliance      = "pci",
+    data_confidentiality = "pub",
+    data_type            = "test",
+    environment          = "dev",
+    gcp_region           = "US",
+    owner                = "hybridenv",
+  }
+
   key_ring = google_kms_key_ring.key_ring.id
 }
 
